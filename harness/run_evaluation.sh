598a40be6dde7451084f31993704e19a36b93e5d
SudharsanSundar <sudharsan.j.sundar@gmail.com>
2024-03-12 02:39:04 -0700

    clarified how to use custom model

--- harness/run_evaluation.sh
+++ harness/run_evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python harness/run_evaluation.py \
-    --predictions_path "outputs/gpt-3.5-turbo-1106__SWE-bench_oracle__test.jsonl" \
+    --predictions_path "prev_runs_outputs/gpt-3.5-turbo-1106__SWE-bench_oracle__test.jsonl" \
     --swe_bench_tasks "swe-bench.json" \
     --log_dir "outputs" \
     --testbed "testbed" \
